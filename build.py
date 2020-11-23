f96c297d3c1e04f0eab5e6fefcb9c549345a2d05
Kasun Hewage <kasun.ch@gmail.com>
2020-11-23 14:42:35 +0100

    new changes

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from cpt_helpers.build_helper import *
 
 if __name__ == "__main__":
     try:
-        builder = ConanMultiPackager(reference="%s/%s" % get_name_and_version(), out=hidesensitive)
+        builder = ConanMultiPackager(out=hidesensitive)
         builder.add_common_builds()
         builder.run()
     except Exception as e:
